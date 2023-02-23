86d819992802331a8e515f1bac9fdff67dc78972
TweakPY <69265406+Filza2@users.noreply.github.com>
2023-02-24 02:28:23 +0300

    Update url-short-learning.py

--- url-short-learning.py
+++ url-short-learning.py
@@ -1,22 +1,8 @@
 import requests
+url=input('Enter URL: ')
+r=requests.get('https://v.ht/api.php?url='+url)
+print(f'''\n
+long/old url : {url}\n
+short/new url : {r.text}\n''')
 
-url=input(
-    
-'Enter URL: '
-
-)
-r=requests.get(
-    'https://v.ht/api.php?url='+url)
-
-print('\n')
-print('long/old url : {}'.format(url))
-print('short/new url : {}'.format(r.text))
-
-#another way to print 
-#print(f'''
-      
-#\n
-#long/old url : {url}
-#short/new url : {r.text}
-
-#''')
+#By @TweakPY - @vv1ck
